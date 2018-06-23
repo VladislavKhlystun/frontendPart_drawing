f71eaf1d5a37ab8fbff1584ffbfcb9e7d279044a
leonid Marchenko <codobred@gmail.com>
2018-06-23 17:49:33 +0300

    draw service, added basic stuff for drawing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { DrawService } from './services/draw.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('risovalka') canvas: ElementRef;
+
+  constructor(private drawService: DrawService) {
   }
+
+  ngAfterViewInit() {
+    // bootstrap draw service
+    this.drawService.setElement(
+      this.canvas.nativeElement
+    );
+  }
+
+  drawLine() {
+    this.drawService.drawLine(0, 100);
+  }
+
+  startDraw(event: MouseEvent) {
+    // get relative coordinates
+    const {x, y} = this.getRelativeCoordinates(event);
+
+    this.drawService.moveTo(x, y);
+    this.drawLine();
+  }
+
+
+  /**
+   * Get coordinates from MouseEvent relative to element
+   * 
+   * @param {MouseEvent} e 
+   * 
+   * @returns relative coordinates
+   */
+  getRelativeCoordinates(e: MouseEvent) {
+    const rect = (<HTMLElement>e.target).getBoundingClientRect();
+    const x = e.clientX - rect.left; //x position within the element.
+    const y = e.clientY - rect.top;  //y position within the element.
+    
+    return {x, y};
+  }
+}
