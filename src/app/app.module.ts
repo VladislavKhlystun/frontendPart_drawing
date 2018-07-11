db2806784900bb7a55321f0ba877890965870d8e
Vladislav <vladislav.khlystun@gmail.com>
2018-07-11 14:48:11 +0300

    added register/auth form components, draw component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PaintComponent } from './paint/paint.component';
+
+const  appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent},
+  { path: '', component: HomeComponent},
+  { path: 'draw', component: PaintComponent}
+] 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    PaintComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,  RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, RegisterComponent]
 })
 export class AppModule { }
---
+++ src/app/paint/paint.component.ts
@@ -0,0 +1,81 @@
+import { OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { DrawService } from '../services/draw.service';
+
+@Component({
+  selector: 'app-paint',
+  templateUrl: './paint.component.html',
+  styleUrls: ['./paint.component.css']
+})
+export class PaintComponent  {
+
+  @ViewChild('risovalka') canvas: ElementRef;
+
+  private is_drawing = false;
+
+  constructor(private drawService: DrawService) {
+  }
+
+  ngAfterViewInit() {
+    // bootstrap draw service
+    this.drawService.setElement(
+      this.canvas.nativeElement
+    );
+  }
+
+  draw(event: MouseEvent) {
+    if (!this.is_drawing) {
+      return;
+    }
+
+    // get relative coordinates on mouse move
+    const {x, y} = this.getRelativeCoordinates(event);
+
+    // TODO change painting method based on drawing tool
+    this.drawService.drawLine(x, y);
+  }
+
+  omMouseUp(event: MouseEvent) {
+    this.moveTo(event);
+    this.is_drawing = false;
+  }
+
+  omMouseDown(event: MouseEvent) {
+    this.moveTo(event);
+    this.is_drawing = true;
+  }
+
+  startDraw(event: MouseEvent) {
+    this.moveTo(event);
+  }
+
+  /**
+   * Move canvas cursor
+   * 
+   * @param {MouseEvent} event
+   * @returns void 
+   */
+  moveTo(event: MouseEvent) {
+    // get relative coordinates
+    const {x, y} = this.getRelativeCoordinates(event);
+
+    this.drawService.moveTo(x, y);
+  }
+
+  /**
+   * Get coordinates from MouseEvent relative to element
+   * 
+   * @param {MouseEvent} e 
+   * 
+   * @returns relative coordinates
+   */
+  getRelativeCoordinates(e: MouseEvent) {
+    const rect = (<HTMLElement>e.target).getBoundingClientRect();
+    const x = e.clientX - rect.left; //x position within the element.
+    const y = e.clientY - rect.top;  //y position within the element.
+    
+    return {x, y};
+  }
+
+
+}
